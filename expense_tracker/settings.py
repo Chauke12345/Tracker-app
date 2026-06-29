5443c3eca50483d8bb326999f790ff8a4fae103c
Eddie <edwardchauke@hotmail.com>
2026-06-29 09:05:44 +0200

    Fix Render deployment settings and login redirect

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -116,6 +116,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # Authentication
-LOGIN_URL = "login"
-LOGIN_REDIRECT_URL = "home"
-LOGOUT_REDIRECT_URL = "login"
+LOGIN_URL = "/login/"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/login/"
